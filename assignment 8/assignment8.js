877c7b99341071b13146d931cac6e02c11c813cb
NamataAngelJulia <angelnamata32@gmail.com>
2021-10-04 20:44:42 +0300

    assignment8

---
+++ assignment 8/assignment8.js
@@ -0,0 +1,28 @@
+function details(Name, age)
+{
+    console.log("Name", Name, "you are" +age+ "years old");
+    
+}
+details("Angel","40");
+details("Julia","35");
+details("Lilly","20");
+details("Ruth","23");
+details("Diana","39");
+
+function age(age)
+{
+    return age;
+}
+function ageIn5years(fiveyearslater)
+{
+    return age+5;
+}
+//arrow function
+let colors =( one, two) => results = one +""+two;
+console.log(colors("red","green"));
+
+
+for( let i=0; i<19; i++)
+{
+    console.log(" I love Uganda");
+}
